27f09c07adf64c45156e51029f8d866f58a6d612
Iuryglabson <45886156+iurysilva@users.noreply.github.com>
2020-11-22 20:03:55 -0300

    Refactor: Simplificando função cria_cromossomos
    A função cria cromossomos agora cria uma lista de cromossomos com bits igual a uma string vazia, uma nova função foi criada chamada gera_posicoes_aleatorias.

--- executar.py
+++ executar.py
@@ -1,16 +1,15 @@
 from objetos import Parametros, Populacao
-import numpy as np
-import bitstring
 
 
 geracoes = 10
-numero_de_cromossomos = 2
+numero_de_cromossomos = 20
 taxa_de_mutacao = 0.80
 taxa_de_crossover = 0.01
 taxa_de_escolha_cromossomo = 0.80
 
 parametros = Parametros(geracoes, taxa_de_mutacao, taxa_de_crossover, taxa_de_escolha_cromossomo)
 populacao = Populacao(numero_de_cromossomos, parametros)
+populacao.gera_posicoes_aleatorias()
 nova_populacao = populacao.seleciona_cromossomos()
 
 for cromossomo in range(0, numero_de_cromossomos):
--- objetos/cromossomo.py
+++ objetos/cromossomo.py
@@ -12,23 +12,19 @@ class Cromossomo:
         fitness = 0
         eachH = {queen + 1: 0 for queen in [col for col in np.arange(8)]}
         for index_da_rainha in range(0, 8):
-            print(self.bits.bin[3 * index_da_rainha:(index_da_rainha + 1 * 3)])
             linha, coluna = index_da_rainha, int(self.bits.bin[3*index_da_rainha:(index_da_rainha+1)*3], 2)
             # verificar a diagonal superior esquerda
             for i, j in zip(range(linha, -1, -1), range(coluna, -1, -1)):
                 # print("Diag acima ",queen," - ", i,j)
                 if self.tabuleiro[i][j] and linha != i and coluna != j:
                     fitness += 1  # soma no custo total
-
                     # soma nos custos individuais
                     eachH[coluna + 1] += 1
                     eachH[j + 1] += 1
-
             # verificar a diagonal inferior esquerda
             for i, j in zip(range(linha, 8, 1), range(coluna, -1, -1)):
                 if self.tabuleiro[i][j] and linha != i and coluna != j:
                     fitness += 1  # soma no custo total
-
                     # soma nos custos individuais
                     eachH[coluna + 1] += 1
                     eachH[j + 1] += 1
